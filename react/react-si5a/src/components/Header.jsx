be1e2f2b37443486b944f2c51a266d31b324d7b1
Daniel Mido <danielmido30@outlook.com>
2025-11-18 15:49:15 +0700

    pertemuan 11/11

---
+++ react/react-si5a/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header( {nama} ) {
+  return (
+    <>
+      <h1>Contoh Component {nama}</h1>
+    </>
+  )
+}
---
+++ react/react-si5a/src/components/Mahasiswa.jsx
@@ -0,0 +1,11 @@
+export default function Mahasiswa( {npm, nama, skill} ) {
+  return (
+    <>
+      <li>
+        <ul>NPM: {npm}</ul>
+        <ul>Nama: {nama}</ul>
+        <ul>Skill: {skill}</ul>
+      </li>
+    </>
+  )
+}
