f6d114f35905cd51d4c3a2b93b5097fc0065b619
Daniel Mido <danielmido30@outlook.com>
2026-01-21 19:13:38 +0700

    update home screen 2

--- react/react-si5a/src/components/Home.jsx
+++ react/react-si5a/src/components/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="container min-vh-100 d-flex align-items-center">
-      <div className="row w-100">
+    <div className="container py-5">
+      <div className="row">
         <div className="col text-center">
           <h1 className="fw-bold mb-3">
             Welcome to the Home Page
